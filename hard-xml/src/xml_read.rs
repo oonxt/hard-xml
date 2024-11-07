be46bc8399f363f63e23ee14700b9ef8c446bb44
oonxt <oonxt@msn.com>
2024-11-07 21:00:38 +0800

    fix bug

--- hard-xml/src/xml_read.rs
+++ hard-xml/src/xml_read.rs
@@ -9,13 +9,15 @@ pub trait XmlRead<'a>: Sized {
     }
     fn from_file(path: impl AsRef<std::path::Path>) -> XmlResult<Self> {
         let text = std::fs::read_to_string(path)?;
-        Self::from_str(&text)
+        let mut reader = XmlReader::new(&text);
+        Self::from_reader(&mut reader)
     }
 
     fn from_buffer<R: std::io::BufRead>(mut buf: R) -> XmlResult<Self> {
         let mut buffer = String::new();
         buf.read_to_string(&mut buffer)?;
-        Self::from_str(&buffer)
+        let mut reader = XmlReader::new(&buffer);
+        Self::from_reader(&mut reader)
     }
 }
 
